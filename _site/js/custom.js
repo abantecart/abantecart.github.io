428e3e0526fbd2eb1138764c3ddc365209196a56
abantecart <projkov@abantecart.com>
2015-08-15 13:07:52 -0400

    Menu improvement and images copy from main site

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 	// Start writing your custom functions here.
 	// All the necessary pluigns are already loaded.
+	
+	//Activate click on submenus with URLs
+	$('.sub_menu_link').on('click',function() {
+		var href = $(this).attr('href');
+		if ( href  && href != '#') {
+			location = href;	
+		}
+	});
+	
+	//Automaticaly open submenus for selected menus
+	$( ".active-submenu" ).each(function() {
+		$(this).find('ul').show();
+	});
+	
 });
